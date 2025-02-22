6f1c747b69754bde60cb2235be99b46841115c40
Arda AY <arda.ay@outlook.com.tr>
2025-02-22 21:32:43 +0300

    Create RegionsController

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:portnumber/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        // GET ALL REGIONS
+        // GET: https://localhost:portnumber/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Region 1",
+                    Code = "Code 1",
+                    RegionImageUrl = "SampleUrl 1"
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Region 2",
+                    Code = "Code 2"
+                }
+            };
+
+            return Ok(regions);
+        }
+    }
+}
