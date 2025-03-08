ba6388c56e8389d0d6796a87eecba8c038781942
Arda AY <arda.ay@outlook.com.tr>
2025-03-08 14:38:03 +0300

    Image Upload Repository Implementation

--- NZWalks/NZWalks.API/Controllers/ImagesController.cs
+++ NZWalks/NZWalks.API/Controllers/ImagesController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
 using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
 
 namespace NZWalks.API.Controllers
 {
@@ -8,6 +11,12 @@ namespace NZWalks.API.Controllers
     [ApiController]
     public class ImagesController : ControllerBase
     {
+        private readonly IImageRepository imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
 
         // POST: /api/Images/Upload
         [HttpPost]
@@ -17,7 +26,20 @@ namespace NZWalks.API.Controllers
             ValidateFileUpload(request);
             if (ModelState.IsValid)
             {
+                // convert DTO ro Domain model
+                var imageDomainModel = new Image
+                {
+                    File = request.File,
+                    FileExtension = Path.GetExtension(request.File.FileName),
+                    FileSizeInBytes = request.File.Length,
+                    FileName = request.FileName,
+                    FileDescription = request.FileDescription,
+                };
+
                 // User repository to upload image
+                await imageRepository.Upload(imageDomainModel);
+
+                return Ok(imageDomainModel);
             }
 
             return BadRequest(ModelState);
@@ -25,7 +47,7 @@ namespace NZWalks.API.Controllers
 
         private void ValidateFileUpload(ImageUploadRequestDto request)
         {
-            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png", ".PNG" };
 
             if (allowedExtensions.Contains(Path.GetExtension(request.File.FileName)) == false)
             {
